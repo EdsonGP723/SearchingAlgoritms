07f648ba1168a901bd86b51669bcb4df34ce86b2
EdsonGP723 <95706015+EdsonGP723@users.noreply.github.com>
2023-10-16 18:58:46 -0600

    Agregando floodfill y set origin

--- Assets/Scripts/DetectCellLocation.cs
+++ Assets/Scripts/DetectCellLocation.cs
@@ -8,6 +8,8 @@ public class DetectCellLocation : MonoBehaviour
     private Vector3 worldPosition;
     public GridLayout gridLayout;
     public Tilemap tilemap;
+    public TileBase origin;
+    public TileBase end;
 
     private void Update()
     {
@@ -16,17 +18,27 @@ public class DetectCellLocation : MonoBehaviour
         worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
         Vector3Int cellPosition = gridLayout.WorldToCell(worldPosition);
         mousePos = gridLayout.CellToWorld(cellPosition);
+        cellPosition.z = 0;
 
         if (Input.GetMouseButtonDown(0))
         {
-
+            var actualTile = tilemap.GetTile(cellPosition);
+            if (actualTile == null) { return; }
             Debug.Log("Origen "+ cellPosition);
+            TileFlags flags = tilemap.GetTileFlags(cellPosition);
+            tilemap.SetTile(cellPosition, origin);
+            tilemap.SetTileFlags(cellPosition, flags);
+            
         }
 
         if (Input.GetMouseButtonDown(1))
         {
-            Debug.Log("Destino " + cellPosition);
             var actualTile = tilemap.GetTile(cellPosition);
+            if (actualTile == null) { return; }
+            Debug.Log("Destino " + cellPosition);
+            TileFlags flags = tilemap.GetTileFlags(cellPosition);
+            tilemap.SetTile(cellPosition, end);
+            tilemap.SetTileFlags(cellPosition, flags);
         }
     }
 }
--- Assets/Scripts/FloodFill.cs
+++ Assets/Scripts/FloodFill.cs
@@ -13,29 +13,65 @@ public class FloodFill : MonoBehaviour
 
     public Tilemap tilemap;
 
-    private void Start()
+    public TileBase tilePurple;
+
+
+
+    private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            FloodFillStartCoroutine();
+        }
+    }
+    public void FloodFillStartCoroutine()
+    {
+
         frontier.Enqueue(startingPoint);
+
+        StartCoroutine(FloodFillCoroutine());
+
+
+    }
+
+    IEnumerator FloodFillCoroutine()
+    {
         while (frontier.Count > 0)
         {
             Vector3Int current = frontier.Dequeue();
 
-            if (tilemap.GetSprite(current)==null) continue;
+            Debug.Log(frontier.Count);
+
 
             List<Vector3Int> neighbours = getNeighbours(current);
-            foreach(Vector3Int next in neighbours)
+
+            foreach (Vector3Int next in neighbours)
             {
-                if (!reached.set.Contains(next))
+
+                if (!reached.set.Contains(next) && tilemap.GetSprite(next) != null)
                 {
-                    frontier.Enqueue(next);
+                    Debug.Log(tilemap.GetSprite(current));
+                    Debug.Log(next);
+                    tilemap.SetTile(next, tilePurple);
                     reached.Add(next);
+                    frontier.Enqueue(next);
+
+
                 }
+
             }
+            yield return new WaitForSeconds(0.01f);
         }
     }
 
     private List<Vector3Int> getNeighbours(Vector3Int current)
     {
-        return new List<Vector3Int>();
+        List<Vector3Int> neighbours = new List<Vector3Int>();
+        neighbours.Add(current + new Vector3Int(0, 1, 0));
+        neighbours.Add(current + new Vector3Int(0, -1, 0));
+        neighbours.Add(current + new Vector3Int(1, 0, 0));
+        neighbours.Add(current + new Vector3Int(-1, 0, 0));
+
+        return neighbours;
     }
 }
